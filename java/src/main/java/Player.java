71cc5f8ff741b56d3b02d2c4098da3ef7efa09f0
Bela <borsy1397@gmail.com>
2019-01-29 19:58:14 +0100

    Add score attribute and initalize to 0, add getters

--- java/src/main/java/Player.java
+++ java/src/main/java/Player.java
@@ -2,9 +2,20 @@
 public class Player {
 	
 	private String name;
+	private int score;
 	
 	Player(String name){
 		this.name = name;
+		this.score = 0;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
 	
 }
