e3281f6ce3a5c16b43bf831d86603801bb41bea1
Bela <borsy1397@gmail.com>
2019-01-30 16:20:36 +0100

    Remove code from conditional statemant of getScore

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -39,48 +39,11 @@ TennisGame1.prototype.wonPoint = function (playerName) {
 
 TennisGame1.prototype.getScore = function () {
     if (this.scores[0] === this.scores[1]) {
-        switch (this.m_score1) {
-            case 0:
-                score = "Love-All";
-                break;
-            case 1:
-                score = "Fifteen-All";
-                break;
-            case 2:
-                score = "Thirty-All";
-                break;
-            default:
-                score = "Deuce";
-                break;
-        }
+        
     } else if (this.scores[0] >= 4 || this.scores[1] >= 4) {
-        var minusResult = this.m_score1 - this.m_score2;
-        if (minusResult === 1) score = "Advantage player1";
-        else if (minusResult === -1) score = "Advantage player2";
-        else if (minusResult >= 2) score = "Win for player1";
-        else score = "Win for player2";
+      
     } else {
-        for (var i = 1; i < 3; i++) {
-            if (i === 1) tempScore = this.m_score1;
-            else {
-                score += "-";
-                tempScore = this.m_score2;
-            }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
-        }
+        
     }
     return score;
 };
